91c66422c7ddf38f6f58f4a600ab1dbdb2d1fe0c
kaploniak <michalkaploniak@gmail.com>
2019-08-08 12:42:07 +0100

    refactored error file

--- errors/index.js
+++ errors/index.js
@@ -1,10 +1,19 @@
 exports.handlePSQLErrors = (err, req, res, next) => {
   // console.log(err, "<----- from error handler");
-  if (err.code === "22P02" || err.code === "23503" || err.code === "23502") {
-    res.status(400).send({ msg: "Bad request" });
-  } else {
-    next(err);
+  const errCodes400 = {
+    "22P02": "Invalid id",
+    "23502": "No data to post!"
+  };
+  const errCodes404 = {
+    "23503": "Page not found"
+  };
+
+  if (errCodes400[err.code]) {
+    res.status(400).send({ msg: errCodes400[err.code] });
+  } else if (errCodes404[err.code]) {
+    res.status(404).send({ msg: errCodes404[err.code] });
   }
+  next(err);
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
