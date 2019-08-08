fcdb0981d5e4aafa1782322cc756784e987b5a3a
kaploniak <michalkaploniak@gmail.com>
2019-08-08 10:49:12 +0100

    added a new path in apiRouter to GET a list of all endpoints

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,6 +3,9 @@ const topicsRouter = require("./topics-router");
 const usersRouter = require("./users-router");
 const articlesRouter = require("./articles-router");
 const commentRouter = require("./comment-router");
+const endPoints = require("../endpoints.json");
+
+apiRouter.route("/").get((req, res) => res.status(200).send(endPoints));
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
