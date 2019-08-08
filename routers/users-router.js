12515a7b28058bdfafebe79d5e5c945c585a291b
kaploniak <michalkaploniak@gmail.com>
2019-08-08 14:50:10 +0100

    added .all() with error handler to all routes to handle invalid method use - user-router

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,6 +1,10 @@
 const usersRouter = require("express").Router();
 const { sendUserByUsername } = require("../controllers/users-controller");
+const { methodNotAllowed } = require("../errors");
 
-usersRouter.route("/:username").get(sendUserByUsername);
+usersRouter
+  .route("/:username")
+  .get(sendUserByUsername)
+  .all(methodNotAllowed);
 
 module.exports = usersRouter;
