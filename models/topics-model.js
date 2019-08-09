5afc2036d4c418307ccc063b48272e3d8dc693de
kaploniak <michalkaploniak@gmail.com>
2019-08-09 13:50:14 +0100

    added createdTopic function to the topic-model

--- models/topics-model.js
+++ models/topics-model.js
@@ -3,3 +3,17 @@ const connection = require("../db/connection");
 exports.selectTopics = () => {
   return connection.select("slug", "description").from("topics");
 };
+
+exports.createdTopic = topic => {
+  return connection
+    .insert(topic)
+    .into("topics")
+    .returning("*")
+    .then(topic => {
+      if (!topic || !topic.length) {
+        return Promise.reject({ status: 404, msg: "Topic not found - test" });
+      } else {
+        return topic;
+      }
+    });
+};
