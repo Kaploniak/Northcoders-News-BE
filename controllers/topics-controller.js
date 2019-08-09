94b000a15b4a49aa7d8e000868a90465bbecb444
kaploniak <michalkaploniak@gmail.com>
2019-08-09 13:49:38 +0100

    added addNewTopic function to the topic-controller

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -1,7 +1,16 @@
-const { selectTopics } = require("../models/topics-model");
+const { selectTopics, createdTopic } = require("../models/topics-model");
 
 exports.sendTopics = (req, res, next) => {
   selectTopics().then(topics => {
     res.status(200).send({ topics });
   });
+};
+
+exports.addNewTopic = (req, res, next) => {
+  const topic = req.body;
+  createdTopic(topic)
+    .then(topic => {
+      res.status(201).send({ topic: topic[0] });
+    })
+    .catch(err => next(err));
 };
