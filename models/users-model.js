6565a594e7f345a9bd224ab68a86d653058532e1
kaploniak <michalkaploniak@gmail.com>
2019-08-09 14:02:38 +0100

    added createdUser function to the user-model

--- models/users-model.js
+++ models/users-model.js
@@ -17,3 +17,17 @@ exports.selectUserByUsername = username => {
     return Promise.reject({ status: 400, message: "Bad request" });
   }
 };
+
+exports.createdUser = user => {
+  return connection
+    .insert(user)
+    .into("users")
+    .returning("*")
+    .then(user => {
+      if (!user || !user.length) {
+        return Promise.reject({ status: 404, msg: "User not found - test" });
+      } else {
+        return user;
+      }
+    });
+};
