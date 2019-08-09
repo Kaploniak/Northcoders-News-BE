3aefeab2d5bcb7617a22500767304ffe0cee1a03
kaploniak <michalkaploniak@gmail.com>
2019-08-09 14:03:15 +0100

    added POST method to the /api/users endpoint

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,10 +1,18 @@
 const usersRouter = require("express").Router();
-const { sendUserByUsername } = require("../controllers/users-controller");
+const {
+  sendUserByUsername,
+  addNewUser
+} = require("../controllers/users-controller");
 const { methodNotAllowed } = require("../errors");
 
 usersRouter
   .route("/:username")
   .get(sendUserByUsername)
+  .all(methodNotAllowed);
+
+usersRouter
+  .route("/")
+  .post(addNewUser)
   .all(methodNotAllowed);
 
 module.exports = usersRouter;
