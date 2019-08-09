a671ff37e787ad284f61d0f6fb0bbbcfe1b0ee3b
kaploniak <michalkaploniak@gmail.com>
2019-08-09 11:49:51 +0100

    added utils finction totalCount to count the articles

--- controllers/utils.js
+++ controllers/utils.js
@@ -9,3 +9,17 @@ exports.checkIfExists = (query, table, column) => {
       else return true;
     });
 };
+
+exports.totalCount = (table, author, topic) => {
+  return connection
+    .select("*")
+    .from(table)
+    .modify(queryBuilder => {
+      if (author) queryBuilder.where("articles.author", author);
+      if (topic) queryBuilder.where("articles.topic", topic);
+    })
+    .then(articles => {
+      if (articles.length === 0) return 0;
+      else return articles.length;
+    });
+};
