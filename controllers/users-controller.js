ad772818c628fcb2ad07c35df53dcd33daf9d2f2
kaploniak <michalkaploniak@gmail.com>
2019-08-09 14:02:05 +0100

    added addNewUser function to the user-controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,10 +1,19 @@
-const { selectUserByUsername } = require("../models/users-model");
+const { selectUserByUsername, createdUser } = require("../models/users-model");
 
 exports.sendUserByUsername = (req, res, next) => {
   const { username } = req.params;
   selectUserByUsername(username)
     .then(user => {
       res.status(200).send({ user: user[0] });
+    })
+    .catch(err => next(err));
+};
+
+exports.addNewUser = (req, res, next) => {
+  const user = req.body;
+  createdUser(user)
+    .then(user => {
+      res.status(201).send({ user: user[0] });
     })
     .catch(err => next(err));
 };
